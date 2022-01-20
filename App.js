8d91b17c360b4092c6c07e5d5fd453c2b20191ab
Barbara Brugger <barbara.brugger@qualiflex.solutions>
2022-01-20 09:37:29 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { View, Text } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <WebView
+      source={{ html: '<iframe width="100%" height="100%" src="https://www.vitabad.ch" frameborder="0" ></iframe>' }}
+      scalesPageToFit={false}
+    />
   );
 }
